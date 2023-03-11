35988c26b38c965134d3a5a8f4827c37d1bbc82b
Ayush Sutariya <127473943+AyushSutariya0@users.noreply.github.com>
2023-03-11 15:56:41 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,1 @@
-# print("hello")
-# firstname = input("enter first name :")
-# lastname = input("enter last name :")
-# print("your name is",firstname,lastname)
-
-import chirag
-
-chirag.ch()
+print("hello")
